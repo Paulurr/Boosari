6b4e8935e00b84b3e9dba1098d826066cb849148
Paulurr <ferpaurr2017@gmail.com>
2026-03-14 21:42:24 -0600

    v 0.0.2

--- resources/js/nav.js
+++ resources/js/nav.js
@@ -1,6 +1,45 @@
 window.addEventListener("DOMContentLoaded",()=>{
+    var nav_menu_button = document.getElementById("nav-menu-button");
     var nav_menu = document.getElementById("nav-menu");
-    nav_menu.addEventListener("click",()=>{
-        alert("");
-    });
-});
+    var nav_menu_back = document.getElementById("nav-menu-back");
+    var path1= document.getElementById("path-nav1");
+    var path2= document.getElementById("path-nav2");
+    var path3= document.getElementById("path-nav3");
+    var open_menu = false;
+    var animation_end = true;
+
+    nav_menu_button.addEventListener("click",open_close_menu);
+    nav_menu_back.addEventListener("click",open_close_menu);
+    function open_close_menu(){    
+        if(!animation_end) return;
+        animation_end = false;
+
+        if ( open_menu == false ){
+            open_menu=true;
+            
+            path1.style.transform ="rotate(-45deg) scaleX(0.7)";
+            path2.style.transform ="translateX(5px)";
+            path3.style.transform ="rotate(45deg) scaleX(0.7)";    
+            path1.style.stroke ="var(--col2)";
+            path2.style.stroke ="var(--col2)";
+            path3.style.stroke ="var(--col2)";    
+            nav_menu.style.transform ="translate(100%)";        
+            nav_menu_back.style.transform ="translate(100%)";        
+        }else{
+            open_menu=false;
+            path1.style.transform ="";
+            path2.style.transform ="";
+            path3.style.transform ="";
+            path1.style.stroke ="";
+            path2.style.stroke ="";
+            path3.style.stroke ="";  
+            nav_menu.style.transform ="";
+            nav_menu_back.style.transform ="";
+        }
+        
+
+        setTimeout(() => {
+            animation_end = true;
+        }, 400);
+    }
+})
--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,23 +1,58 @@
 @vite(['resources/js/nav.js'])
 <div class="h-15 w-auto flex flex-col mt-2 select-none">
     <div class="flex h-11/12 w-auto">
-        <div class="h-full lg:w-1/4 w-full flex items-center justify-center pl-4 pr-4 lg:pl-0 lg:pr-0">
+        <div class="h-full lg:w-1/4 w-full flex items-center justify-center pl-4 pr-4 lg:pl-0 sm:pr-0">
 
             <div class="nav-menu-cont flex items-center h-full w-1/3 lg:hidden">
-                <button class="nav-menu cursor-pointer" id="nav-menu">
+                <div class="h-full w-full absolute top-0 flex nav-menu-back right-full flex-col" id="nav-menu-back"></div>
+                <div class="h-full w-1/3 absolute top-0 right-3/3 flex nav-menu flex-col" id="nav-menu">
+                    <div class="h-15 w-full"></div>
+                    <ul class="ul-menu-nav flex flex-col h-full p-3">
+                        <li class="li-menu-nav rounded-br-md rounded-tr-md overflow-hidden">
+                            <a href="" class="a-menu-nav text-sm p-2">
+                                Inicio
+                            </a>
+                        </li>
+                        <li class="li-menu-nav rounded-br-md rounded-tr-md overflow-hidden">
+                            <a href="" class="a-menu-nav text-sm p-2">
+                                Sobre nosotros
+                            </a>
+                            
+                        </li>
+                        </li>
+                        <li class="li-menu-nav rounded-br-md rounded-tr-md overflow-hidden">
+                            <a href="" class="a-menu-nav text-sm p-2">
+                                Acerca de
+                            </a>
+                            
+                        </li>
+                    </ul>
+                </div>
+                <button class="nav-menu-button p-2 cursor-pointer relative rounded-full" id="nav-menu-button">
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M3 6H21" stroke="black" stroke-width="2" stroke-linecap="round"/>
-                        <path d="M3 12H21" stroke="black" stroke-width="2" stroke-linecap="round"/>
-                        <path d="M3 18H21" stroke="black" stroke-width="2" stroke-linecap="round"/>
+                        <path d="M3 6H21" stroke="black" stroke-width="2" stroke-linecap="round" id="path-nav1"/>
+                        <path d="M3 12H21" stroke="black" stroke-width="2" stroke-linecap="round" id="path-nav2"/>
+                        <path d="M3 18H21" stroke="black" stroke-width="2" stroke-linecap="round" id="path-nav3"/>
                     </svg>
                 </button>
             </div>
-            <div class="flex justify-center h-full w-1/3">
+            <a href="" class="flex justify-center h-full w-1/3">
                 <img src="{{ asset('images/logotipo_boosari.webp') }}" alt="Logo" srcset="" class="h-10/12 object-contain">
-            </div>
-            <div class="flex items-center justify-end h-full w-1/3">
+            </a>
+            <ul class="ul-nav-menu flex items-center  justify-end sm:justify-evenly h-full w-1/3">
+                <li class="li-nav">
+                    <a href="" class="a-nav a-nav-log p-1 rounded-md overflow-hidden text-sm lg:text-lg" >
+                        Iniciar Sesión
+                    </a>
                     
-            </div>
+                </li>
+                <li class="li-nav">
+                    <a href="" class="a-nav a-nav-up p-1 hidden sm:flex rounded-md overflow-hidden text-sm lg:text-lg" >
+                        Registrarse
+                    </a>
+                    
+                </li>
+            </ul>
         </div>
         <ul class="ul-nav h-full w-[55%] flex items-center justify-end 100">
             <li class="li-nav">
@@ -42,14 +77,14 @@
         </ul>
         <ul class="ul-nav h-full w-1/4 flex items-center justify-end">
             <li class="li-nav">
-                <a href="" class="a-nav">
+                <a href="" class="a-nav a-nav-log p-1 rounded-md overflow-hidden" >
                     Iniciar Sesión
                 </a>
                 
             </li>
             <li class="li-nav">
-                <a href="" class="a-nav">
-                    Cerrar Sesión
+                <a href="" class="a-nav a-nav-up p-1 rounded-md overflow-hidden">
+                    Registrarse
                 </a>
             </li>
         </ul>
